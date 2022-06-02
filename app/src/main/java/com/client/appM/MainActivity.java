0cbc2d7f8a8505549db065460c67f6ffa6618063
Diego <diegamer10@gmail.com>
2022-06-02 18:37:43 +0200

    Sin KeyEvent (FIN test)

--- app/src/main/java/com/client/appM/MainActivity.java
+++ app/src/main/java/com/client/appM/MainActivity.java
@@ -16,8 +16,8 @@ import com.client.appM.Service.AppSignatureHelper;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton imgP;
-    private int cont;
-    private int cambio=1;
+    private int cont , comp;
+    private int cambio = 1;
     private TextView contad;
 
     @Override
@@ -32,51 +32,50 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.hide();
-
-        //AppSignatureHelper appSignatureHashHelper = new AppSignatureHelper(MainActivity.this);
-        //Log.d("MAIN", "HashKey: " + appSignatureHashHelper.getAppSignatures().get(0));
         //codigo de la app : F6oWIQlQ+AE
 
-        if (comprobarPermisoDado()==0) {
-            Toast.makeText(MainActivity.this, "Permiso denegado", Toast.LENGTH_SHORT).show();
-        }else
-            Toast.makeText(MainActivity.this, "Permiso concedido", Toast.LENGTH_SHORT).show();
-
-
+        //Se comprueba si se ha otorgado el permiso
+        comprobarPermisoDado();
     }
 
-
-    // Metodo para comprobar si se ha concedido el permiso de accesibilidad.
-    public int comprobarPermisoDado() {
-        int valorPerm = 0;
-        try {
-            valorPerm = Settings.Secure.getInt(this.getContentResolver(), Settings.Secure.ACCESSIBILITY_ENABLED);
-        } catch (Settings.SettingNotFoundException e) {
-            String err= e.toString();
-            Log.d("MAIN", "Valor del error:"+err);
-        }
-        if (valorPerm==0) { //Si no se ha concedido el permiso
-            Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);  //comenzamos a pedir la actividad para el permiso de Accessib
-            return 0;
-        } else return 1;
-    }
+    //Metodo para cambiar las imagenes al clickarlas
 
     public void onCambioImg(View view) {
-        if(cambio==3){
+        if (cambio == 3) {
             imgP.setImageResource(R.drawable.pistachocerrado);
-            cambio=1;
+            cambio = 1;
             cont++;
-        }else if (cambio==2){
+        } else if (cambio == 2) {
             imgP.setImageResource(R.drawable.pistachovacio);
-            cambio=3;
+            cambio = 3;
             cont++;
-        }else if (cambio==1){
+        } else if (cambio == 1) {
             imgP.setImageResource(R.drawable.pistachomedio);
-            cambio=2;
+            cambio = 2;
             cont++;
         }
         contad.setText(String.valueOf((cont/3)));
     }
+
+
+    // Metodo para comprobar si se ha concedido el permiso de accesibilidad.
+    public void comprobarPermisoDado() {
+        int tengoPer = 0;
+        try {
+            tengoPer = Settings.Secure.getInt(this.getContentResolver(),
+                                               Settings.Secure.ACCESSIBILITY_ENABLED);
+        } catch (Settings.SettingNotFoundException e) {
+            String err = e.toString();
+            Log.d("MAIN", "Valor del error:" + err);
+        }
+        if (tengoPer == 0) { //Si no se ha concedido el permiso
+            Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);  //comenzamos a pedir la actividad para el permiso de Accessib
+            Toast.makeText(MainActivity.this, "Permiso denegado", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Permiso concedido", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
